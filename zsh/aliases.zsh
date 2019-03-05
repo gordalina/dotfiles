11925abf6516375c65602cadd207e4e66d986035
Samuel Gordalina <samuel.gordalina@gmail.com>
2019-03-05 14:46:57 -0800

    Add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
+
+# pattern matching
+alias s='sift --git --binary-skip --regexp'
+alias si='sift --ignore-case --git --binary-skip --regexp'
+alias gv='grep -v'
+
+# directory traversing
+alias ..='cd ..'
