07197fc8b5d7ce72bb20a55b5d7d2b352a3ce5ae
Samuel Gordalina <samuel.gordalina@gmail.com>
2019-03-05 16:11:24 -0800

    Add more pattern matching aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,16 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 # pattern matching
-alias s='sift --git --binary-skip --regexp'
-alias si='sift --ignore-case --git --binary-skip --regexp'
+alias s='sift --git --binary-skip'
+alias se='sift --git --binary-skip --regexp'
+alias si='sift --ignore-case --git --binary-skip'
+alias sei='sift --ignore-case --git --binary-skip --regexp'
+alias sf='find . | s'
+alias sfi='find . | si'
+alias sfe='find . | se'
+alias sfei='find . | sei'
 alias gv='grep -v'
 
 # directory traversing
 alias ..='cd ..'
+alias ...='cd ../..'
