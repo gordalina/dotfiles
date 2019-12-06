d305367cd3223269209c3a7317ee39a451978cbc
Samuel Gordalina <samuel.gordalina@gmail.com>
2019-12-05 16:51:21 -0800

    fix gcp

--- gcp/gcloud.zsh
+++ gcp/gcloud.zsh
@@ -1,7 +1,7 @@
-$GCLOUD_PATH="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+GCLOUD_PATH="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "$GCLOUD_PATH/path.zsh.inc" ]; then . '/usr/local/opt/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f "$GCLOUD_PATH/path.zsh.inc" ]; then . "$GCLOUD_PATH/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f "$GCLOUD_PATH/completion.zsh.inc" ]; then . '/usr/local/opt/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f "$GCLOUD_PATH/completion.zsh.inc" ]; then . "$GCLOUD_PATH/completion.zsh.inc"; fi
