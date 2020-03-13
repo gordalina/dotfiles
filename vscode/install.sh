39e503f142012eb5e7995ad0b5059a090ff06b35
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-03-13 13:36:31 -0700

    Add vscode

---
+++ vscode/install.sh
@@ -0,0 +1,7 @@
+# create directory
+mkdir -p ~/Library/Application\ Support/Code/User/
+
+# create symlink
+ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.dotfiles/vscode/snippets/ ~/Library/Application\ Support/Code/User
