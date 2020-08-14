fc48193197048a07b533bb4d62b0b7ced165c459
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-08-13 20:04:13 -0700

    Update keybindings for thirds & option vs alt

--- phoenix/src/key-bindings.js
+++ phoenix/src/key-bindings.js
@@ -1,31 +1,31 @@
 const handles = {
   // window position in screen
-  left: new Key('left', ['ctrl', 'alt'], () =>
+  left: new Key('left', ['ctrl', 'option'], () =>
     setFrame(getPosition('left'))),
-  right: new Key('right', ['ctrl', 'alt'], () =>
+  right: new Key('right', ['ctrl', 'option'], () =>
     setFrame(getPosition('right'))),
 
   // halve the window height
-  top_half: new Key('up', ['ctrl', 'alt'], () =>
+  top_half: new Key('up', ['ctrl', 'option'], () =>
     setAbsoluteFrame(getAbsolutePosition('top-half'))),
-  bottom_half: new Key('down', ['ctrl', 'alt'], () =>
+  bottom_half: new Key('down', ['ctrl', 'option'], () =>
     setAbsoluteFrame(getAbsolutePosition('bottom-half'))),
 
   // maximize
-  maximize: new Key('m', ['ctrl', 'alt'], () =>
+  maximize: new Key('m', ['ctrl', 'option'], () =>
     Window.focused().maximize()),
 
   // thirds
-  t1: new Key('1', ['ctrl', 'alt'], () =>
+  t1: new Key('forwardDelete', ['ctrl', 'option'], () =>
     setFrame(getPosition('left-third'))),
-  t2: new Key('2', ['ctrl', 'alt'], () =>
+  t2: new Key('end', ['ctrl', 'option'], () =>
     setFrame(getPosition('center-third'))),
-  t3: new Key('3', ['ctrl', 'alt'], () =>
+  t3: new Key('pageDown', ['ctrl', 'option'], () =>
     setFrame(getPosition('right-third'))),
 
   // move window between screens
-  leftScreen: new Key('left', ['ctrl', 'alt', 'cmd'], () =>
-    moveToScreen({ offset: 1 })),
-  rightScreen: new Key('right', ['ctrl', 'alt', 'cmd'], () =>
+  leftScreen: new Key('left', ['ctrl', 'option', 'cmd'], () =>
     moveToScreen({ offset: -1 })),
+  rightScreen: new Key('right', ['ctrl', 'option', 'cmd'], () =>
+    moveToScreen({ offset: 1 })),
 };
