654d2807855f600f4cbdefacbffebb828f235566
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-11-20 18:11:53 -0800

    Updates

--- gcp/gcloud.zsh
+++ gcp/gcloud.zsh
@@ -1,7 +1,4 @@
-GCLOUD_PATH="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$GCLOUD_PATH/path.zsh.inc" ]; then . "$GCLOUD_PATH/path.zsh.inc"; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f "$GCLOUD_PATH/completion.zsh.inc" ]; then . "$GCLOUD_PATH/completion.zsh.inc"; fi
+export GCLOUD_PATH="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+export CLOUDSDK_PYTHON="/usr/local/opt/python@3.8/libexec/bin/python"
+source "${GCLOUD_PATH}/path.zsh.inc"
+source "${GCLOUD_PATH}/completion.zsh.inc"
