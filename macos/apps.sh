5e5e67211c4684e1a72fcc93ba1c325b75d5f8ef
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-12-03 10:05:11 -0800

    Set default apps

---
+++ macos/apps.sh
@@ -0,0 +1,13 @@
+# Bartender
+
+# Do not hide menu items when showing hidden ones
+defaults write com.surteesstudios.Bartender.plist HideItemsWhenShowingOthers -int 0
+
+# Prevent menu bar items from graying out on non-active monitor
+defaults write com.surteesstudios.Bartender.plist HideSecondaryMenuBarItems -int 0
+
+# Show hidden items on hover
+defaults write com.surteesstudios.Bartender.plist MouseOverMenuBarTogglesBartender -int 1
+
+# Show for Updates
+defaults write com.surteesstudios.Bartender.plist DisableAllSHowForUpdates -int 1
