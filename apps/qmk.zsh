ec520a1ac34bddcb4b3cf10ae96dc40f24c5f518
Samuel Gordalina <samuel.gordalina@gmail.com>
2021-03-27 18:55:26 -0700

    Add QMK

---
+++ apps/qmk.zsh
@@ -0,0 +1,7 @@
+# avr-gcc
+export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/avr-gcc@8/lib"
+
+# arm-gcc-bin
+export PATH="/usr/local/opt/arm-gcc-bin@8/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/arm-gcc-bin@8/lib"
