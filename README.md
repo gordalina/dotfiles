746b1137ae73e14b92bc995a5452f5495ade99e2
Samuel Gordalina <samuel.gordalina@gmail.com>
2026-07-11 18:35:12 -1000

    updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# gordaline does dotfiles
+# gordalina does dotfiles
 
 ```
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/gordalina/dotfiles/HEAD/install.sh)"
--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
 
+set -euxo pipefail
+
+echo "› install developer tools"
+sudo xcode-select --install
+
 echo "› install homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NOINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 
 echo "› cloning dotfiles"
 mkdir -p ~/src
-git clone git@github.com:gordalina/dotfiles.git ~/src/dotfiles
-cd dotfiles
+git clone https://github.com/gordalina/dotfiles ~/src/dotfiles
+cd ~/src/dotfiles
 
 echo "› bootstrap dotfiles"
-brew install mise
+brew install mise --yes
 mise bootstrap
 
 echo "› applying defaults"
